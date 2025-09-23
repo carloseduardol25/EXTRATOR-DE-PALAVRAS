34805f9f90657df2773a703acca87fb9637dc3d0
carloseduardol25 <carlos.leineker@escola.pr.gov.br>
2025-09-23 09:35:01 -0300

    4

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-
 const botaoMostrapalavras = document.querySelector("#botao-palavrachave");
 
 botaomostrapalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
-
      const campoResultado = document.querySelector("#resultado-palavrachave");
-
-     const palavraschave= processaTexto(texto);
-
-     campoResultado.textcontent = palavras.join(",");
+    const palavraschave= processaTexto(texto);
+     
+    campoResultado.textcontent = palavras.join(",");
 }
 
 function processaTexto(texto) {
     let palvras = texto.split(/\P{L}+/u);
+    
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias{1} =0; 
+        for(let j of paavras) {
+            if(i==j) {
+                frequencia[i]++; 
+            }
+        }
+        
     return palavras;
 }
 
