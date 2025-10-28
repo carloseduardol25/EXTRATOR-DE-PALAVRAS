b8d416b271a621554bb4a188c3ff98f867f819ec
carloseduardol25 <carlos.leineker@escola.pr.gov.br>
2025-10-28 08:54:03 -0300

    aula 8

--- script.js
+++ script.js
@@ -1,38 +1,179 @@
-const botaoMostrapalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector( "#botao-paçavrachave");
 
-botaomostrapalavras.addEventListener("click", mostraPalavrasChave);
+botaoMostraPalavras.addEventListener( " click", mostraPalavrasChave);
 
-function mostraPalavrasChave() {
-    const texto = document.querySelector("#entrada-de-texto").value;
-     const campoResultado = document.querySelector("#resultado-palavrachave");
-    const palavraschave= processaTexto(texto);
-     
-    campoResultado.textcontent = palavras.join(",");
-}
+function mostraPalavrasChave(){ 
+    const texto = document.querySelector( "#entrada-de-texto").ariaValueMax;
+    const campoResultado = document. querySelector( "#resultado-palavrachave");
+    const mostraPalavrasChave = processaTexto(texto);
 
+    campoResultado.textoContent = palavras.join(","); 
+}    
+💡
 function processaTexto(texto) {
-    let palvras = texto.split(/\P{L}+/u);
-    const frequencias = contafrequencias(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenapalavra);
+    let palavras = texto.split(/\P{L}+/u);
 
-    function ordenapalavra(p1, p2){
-        return frequencias[p2];
+    for (let i of palavras) ; {
+        palavras[i] = palavras[i].toLwerCase();
     }
     
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
+
+    function ordenaPalavras(p1, p2){
+        return frequencias[p2] - frequencias[p1];
+    }
+
     console.log(ordenadas);
-    return ordenadas.slice(0, 10);
-    
-function contaFrequencias(palavras)
+    return ordenadas.slice(0,10);
+}
+💡
+function contaFrequencias(palavras){
     let frequencias = {};
     for (let i of palavras) {
-        frequencias{1} =0; 
-        for(let j of paavras) {
-            if(i==j) {
-                frequencia[i]++; 
-            }
+        frequencias [i] =0; 
+    for(let j of palavras){
+        if(i==j) {
+            frequencias[i]++;
         }
-    }     
+        }
+    }
     return palavras;
+}
+💡
+function tiraPalavrasRuins(palavra) {
+   const PALAVRAS_RUINS = new Set([
+    "que",
+    "para",
+    "com",
+    "não",
+    "uma",
+    "por",
+    "mais",
+    "dos",
+    "como",
+    "mas",
+    "foi",
+    "ele",
+    "das",
+    "tem",
+    "seu",
+    "sua",
+    "ser",
+    "quando",
+    "muito",
+    "está",
+    "também",
+    "pelo",
+    "pela",
+    "até",
+    "isso",
+    "ela",
+    "entre",
+    "era",
+    "depois",
+    "sem",
+    "mesmo",
+    "aos",
+    "ter",
+    "seus",
+    "quem",
+    "nas",
+    "esse",
+    "eles",
+    "estão",
+    "você",
+    "tinha",
+    "foram",
+    "essa",
+    "num",
+    "nem",
+    "suas",
+    "meu",
+    "minha",
+    "têm",
+    "numa",
+    "pelos",
+    "elas",
+    "havia",
+    "seja",
+    "qual",
+    "será",
+    "nós",
+    "tenho",
+    "lhe",
+    "deles",
+    "essas",
+    "esses",
+    "pelas",
+    "este",
+    "fosse",
+    "dele",
+    "vocês",
+    "vos",
+    "lhes",
+    "meus",
+    "minhas",
+    "teu",
+    "tua",
+    "teus",
+    "tuas",
+    "nosso",
+    "nossa",
+    "nossos",
+    "nossas",
+    "dela",
+    "delas",
+    "esta",
+    "estes",
+    "estas",
+    "aquele",
+    "aquela",
+    "aqueles",
+    "aquelas",
+    "isto",
+    "aquilo",
+    "estou",
+    "estamos",
+    "estive",
+    "esteve",
+    "estivemos",
+    "estiveram",
+    "estava",
+    "estávamos",
+    "estavam",
+    "e",
+    "ou",
+    "onde",
+    "aquilo",
+    "embora",
+    "apesar",
+    "porque",
+    "enquanto",
+    "contudo",
+    "entretanto",
+    "portanto",
+    "além",
+    "antes",
+    "todavia",
+    "inclusive",
+    "diante",
+    "sobre",
+    "quanto",
+    "apenas",
+    "desde",
+    "uma vez",
+    "segundo",
+    "junto"
+]);
 
-
-
+    const palavrasBoas = [];
+    for (let palavras of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+             palavrasBoas. push(palavra);
+        }
+    }
+    retorn palavrasBoas;
+}   
